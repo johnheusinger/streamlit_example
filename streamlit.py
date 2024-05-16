1032af451208c0f4ce322ef4d3157660a4246f7a
John Heusinger <john.heusinger@gmail.com>
2024-05-16 11:38:33 +0200

    Add sidebar

--- streamlit.py
+++ streamlit.py
@@ -15,5 +15,16 @@ def main():
     video_url = "https://www.youtube.com/watch?v=y8Kyi0WNg40"
     st.video(video_url)
 
+    #Sidebar
+    st.sidebar.write("This is a sidebar.")
+
+    #Add two images to the sidebar, with a radio button to switch between them
+
+    image1 = "happy.jpg"
+    image2 = "sad.jpg"
+    image_choice = st.sidebar.radio("Choose an image.", [image1, image2])
+
+    st.sidebar.image(image_choice)
+
 if __name__ == "__main__":
     main()
